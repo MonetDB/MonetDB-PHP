87109cfe3a7a9deb9fd1483e4232446babef77fa
MitchellWeg <mitchell.w@live.nl>
2020-12-16 16:42:17 +0100

    add tests for dec38

--- tests/dec38Test.php
+++ tests/dec38Test.php
@@ -1,30 +1,40 @@
 <?php
-// require 'monetdb/php_monetdb.php';
+use PHPUnit\Framework\TestCase;
+use MonetDB\Connection;
 
-// $db = monetdb_connect("sql", "localhost", $argv[1], "monetdb", "monetdb", $argv[2]) or die(monetdb_last_error());
+require_once(__DIR__. '../../src/include.php');
 
-// $res = monetdb_query('START TRANSACTION;') or die(monetdb_last_error());
-// while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
+final class dec38Test extends TestCase {
+    public $conn;
 
-// $res = monetdb_query('CREATE TABLE php_dec38 (d38_0 DECIMAL(38,0), d38_19 DECIMAL(38,19), d38_38 DECIMAL(38,38));') or die(monetdb_last_error());
-// while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
+    public function setUp(): void
+    {
+        $this->conn = new Connection("127.0.0.1", 50000, "monetdb", "monetdb", "temp");
+    }
 
-// $res = monetdb_query('INSERT INTO php_dec38 VALUES (12345678901234567899876543210987654321, 1234567890123456789.9876543210987654321, .12345678901234567899876543210987654321);') or die(monetdb_last_error());
-// while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
+    public function testBigIntTable(): void 
+    {
+        $res = $this->conn->Query("CREATE TABLE php_dec38 (d38_0 DECIMAL(38,0), d38_19 DECIMAL(38,19), d38_38 DECIMAL(38,38));");
 
-// $res = monetdb_query('SELECT * FROM php_dec38;') or die(monetdb_last_error());
-// while ( $row = monetdb_fetch_assoc($res) ) { print_r($row); }
+        $this->assertCount(0, $res);
+    }
 
-// $res = monetdb_query('SELECT * FROM php_dec38;') or die(monetdb_last_error());
-// $cols = monetdb_num_fields($res);
-// for ($i = 0; $i < $cols; $i++) {
-// 	print(monetdb_field_name($res, $i)."\t");
-// }
-// print("\n");
-// while ($row = @monetdb_fetch_row($res)) {
-// 	for ($i = 0; $i < $cols; $i++) {
-// 		print($row[$i]."\t");
-// 	}
-// 	print("\n");
-// }
+    public function testInsertBigInt(): void 
+    {
+        $res = $this->conn->Query("INSERT INTO php_dec38 VALUES (12345678901234567899876543210987654321, 1234567890123456789.9876543210987654321, .12345678901234567899876543210987654321);");
+
+        $this->assertCount(0, $res);
+    }
+
+    public function testSelectBigInt(): void 
+    {
+        $res = $this->conn->Query("SELECT * FROM php_dec38");
+        $res_arr = iterator_to_array($res);
+
+        $this->assertEquals($res_arr[1]["d38_0"], "12345678901234567899876543210987654321");
+        $this->assertEquals($res_arr[1]["d38_19"], "1234567890123456789.9876543210987654321");
+        $this->assertEquals($res_arr[1]["d38_38"], "0.12345678901234567899876543210987654321");
+    }
+
+}
 ?>
